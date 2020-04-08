0c88aa35a37903c29d6458f6378bdf8887474cf2
xinqiymsz <pangxinqi@js.com>
2020-04-08 14:03:15 +0800

    add

--- lala.js
+++ lala.js
@@ -1,11 +1,11 @@
 
 const Promise = require('./promise');
-var a = new Promise((resolve) => {
-    resolve(1);
-});
-var b = new Promise((resolve, reject) => {
-    reject(3);
-});
+// var a = new Promise((resolve) => {
+//     resolve(1);
+// });
+// var b = new Promise((resolve, reject) => {
+//     reject(3);
+// });
 // a.then((res) => {
 //   return new Promise((resolve) => { // return个promise
 //     setTimeout(() => {resolve(233)}, 2000);
@@ -17,4 +17,17 @@ Promise.all([a,b]).then((res) => {
   console.log(res, '////res');
 }, (err) => {
   console.log(err, '/////err');
+});
+var a = new Promise1(resolve => resolve(4));
+var b = new Promise1(resolve => resolve(5));
+var d = new Promise1(resolve => resolve(6));
+
+// 比如登录失效的情况下就可能有如下场景 首先失败的接口会在then里面再次请求登录接口 请求成功后会重新调用刚刚的请求
+
+var c = a.then((a)=> {
+    return b.then((b)=> {
+        return d.then((d)=> {
+            return a + b + d;
+        });
+    });
 });
